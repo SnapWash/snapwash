e7286fe5215177d775f7244530eb3c866c90d1b3
SnapWash <kelompok4.4ti7.dpp.web.2025@gmail.com>
2025-05-04 10:30:47 +0800

    Add files via upload
    Signed-off-by: SnapWash <kelompok4.4ti7.dpp.web.2025@gmail.com>

---
+++ assets/js/contact.js
@@ -0,0 +1,6 @@
+function validateForm() {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value;
+    
+}
